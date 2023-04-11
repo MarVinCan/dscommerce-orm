a70583175c3f06148b92382feff77a934d191c24
Marcus Vinicius <marcus.v.e.candido@gmail.com>
2023-04-11 15:09:14 -0300

    Start project/ entities: User/Order/ enum: OrderStatus + test H2

---
+++ src/main/java/com/marcus/dscommerce/entities/OrderStatus.java
@@ -0,0 +1,10 @@
+package com.marcus.dscommerce.entities;
+
+public enum OrderStatus {
+    
+    WAITING_PAYMENT,
+    PAID,
+    SHIPPED,
+    DELIVERED,
+    CANCELED;
+}
---
+++ src/test/java/com/marcus/dscommerce/DscommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcus.dscommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DscommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
