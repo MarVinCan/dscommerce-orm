b2889b8ba48bf3c39b3ca708e84bd13f4650d9cc
Marcus Vinicius <marcus.v.e.candido@gmail.com>
2023-04-14 15:15:35 -0300

    creating DTO, structuring layers / get product by id

--- src/main/java/com/marcus/dscommerce/controllers/ProductController.java
+++ src/main/java/com/marcus/dscommerce/controllers/ProductController.java
@@ -1,28 +1,25 @@
 package com.marcus.dscommerce.controllers;
 
-import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.marcus.dscommerce.entities.Product;
-import com.marcus.dscommerce.repositories.ProductRepository;
+import com.marcus.dscommerce.dto.ProductDTO;
+import com.marcus.dscommerce.services.ProductService;
 
 @RestController
 @RequestMapping(value = "/products")
 public class ProductController {
 
-
     @Autowired
-    private ProductRepository repository;
+    private ProductService service;
     
-    @GetMapping
-    public String teste(){
-       Optional<Product> result = repository.findById(1L);
-       Product product = result.get();
-       return product.getName();
+    @GetMapping(value = "/{id}")
+    public ProductDTO findById(@PathVariable Long id){
+       ProductDTO dto = service.findById(id);
+       return dto;
     }
     
 
