d6b63d63ffd9370c883e1719263446cef4539068
Marcus Vinicius <marcus.v.e.candido@gmail.com>
2023-04-14 15:52:40 -0300

    Get product paged

--- src/main/java/com/marcus/dscommerce/controllers/ProductController.java
+++ src/main/java/com/marcus/dscommerce/controllers/ProductController.java
@@ -1,6 +1,10 @@
 package com.marcus.dscommerce.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,5 +26,9 @@ public class ProductController {
        return dto;
     }
     
+    @GetMapping
+    public Page<ProductDTO> findAll(Pageable pageable){
+       return service.findAll(pageable);
+    }
 
 }
