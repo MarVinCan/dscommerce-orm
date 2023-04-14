7464db7179156f9ed9758fb06a999480d0110199
Marcus Vinicius <marcus.v.e.candido@gmail.com>
2023-04-14 14:49:21 -0300

    first test with Repository

--- src/main/java/com/marcus/dscommerce/controllers/ProductController.java
+++ src/main/java/com/marcus/dscommerce/controllers/ProductController.java
@@ -1,16 +1,28 @@
 package com.marcus.dscommerce.controllers;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.marcus.dscommerce.entities.Product;
+import com.marcus.dscommerce.repositories.ProductRepository;
 
 @RestController
 @RequestMapping(value = "/products")
 public class ProductController {
 
+
+    @Autowired
+    private ProductRepository repository;
+    
     @GetMapping
     public String teste(){
-        return "Olá Mundo!";
+       Optional<Product> result = repository.findById(1L);
+       Product product = result.get();
+       return product.getName();
     }
     
 
---
+++ src/main/java/com/marcus/dscommerce/repositories/ProductRepository.java
@@ -0,0 +1,8 @@
+package com.marcus.dscommerce.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.marcus.dscommerce.entities.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
