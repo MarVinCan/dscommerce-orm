af5f37c7ab7eb286d6524f564d1b6c802f8fa041
Marcus Vinicius <marcus.v.e.candido@gmail.com>
2023-05-03 16:22:56 -0300

    Encoded password storage

--- src/main/java/com/marcus/dscommerce/repositories/UserRepository.java
+++ src/main/java/com/marcus/dscommerce/repositories/UserRepository.java
@@ -1,14 +1,9 @@
 package com.marcus.dscommerce.repositories;
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-
-import com.marcus.dscommerce.entities.Product;
 import com.marcus.dscommerce.entities.User;
 
-public interface UserRepository extends JpaRepository<Product, Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
 
     User findByEmail(String email);
 
