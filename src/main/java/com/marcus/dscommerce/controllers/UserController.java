2ee6f172fb193ceed399162eb4f10c71ceacf7ff
Marcus Vinicius <marcus.v.e.candido@gmail.com>
2023-05-04 17:00:23 -0300

    Getting user logged

---
+++ src/main/java/com/marcus/dscommerce/controllers/UserController.java
@@ -0,0 +1,30 @@
+package com.marcus.dscommerce.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.marcus.dscommerce.dto.UserDTO;
+import com.marcus.dscommerce.services.UserService;
+
+
+
+@RestController
+@RequestMapping(value = "/user")
+public class UserController {
+
+    @Autowired
+    private UserService service;
+    
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_CLIENT')")
+    @GetMapping(value = "/me")
+    public ResponseEntity<UserDTO> getMe(){
+       UserDTO dto = service.getMe();
+       return ResponseEntity.ok(dto);
+    }
+    
+   
+
+}
